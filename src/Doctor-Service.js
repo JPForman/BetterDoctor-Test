7fd51a9014119f04181511bf3b29b472cc5eed71
Joshua Forman <cyborgforman@gmail.com>
2019-11-22 10:13:54 -0800

    add logic for gathering api

---
+++ src/Doctor-Service.js
@@ -0,0 +1,14 @@
+export class DoctorService {
+  async getDoctors(art) {
+    try {
+      let response = await fetch(`https://api.harvardartmuseums.org/image?apikey=${process.env.API_KEY}&size=3&sort=random&baseimageurl`);
+      let jsonifiedResponse = await response.json();
+      return jsonifiedResponse;
+    } catch(error) {
+      //add error message to user here
+      console.error("There was an error handling your request: " + error.message);
+
+
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-// import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+(async () => {
+  let doctorService = new DoctorService();
+  const response = await doctorService.getDoctors();
+  // getElements(response);
+})();
