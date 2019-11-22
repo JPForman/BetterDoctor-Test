1837346f834c3e3d456a4c2e4fd0c5b6b9230a2a
Joshua Forman <cyborgforman@gmail.com>
2019-11-22 10:27:59 -0800

    add ui logic to gather input data

--- src/main.js
+++ src/main.js
@@ -3,8 +3,19 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-(async () => {
-  let doctorService = new DoctorService();
-  const response = await doctorService.getDoctors();
-  // getElements(response);
-})();
+
+$(document).ready(function(){
+  $('form#issueForm').submit(function(event){
+    event.preventDefault();
+    let issue = $('#issueInput').val();
+
+
+  })
+
+  (async () => {
+    let doctorService = new DoctorService();
+    const response = await doctorService.getDoctors(issue);
+    // getElements(response);
+  })();
+
+})
