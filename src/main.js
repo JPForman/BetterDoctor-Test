3851ce29e4974641b2a96657a2163a21961c1755
Joshua Forman <cyborgforman@gmail.com>
2019-11-22 13:31:27 -0800

    add functional retrieval and output of address"

--- src/main.js
+++ src/main.js
@@ -18,7 +18,11 @@ $(document).ready(function(){
 
     function outputElements(doctorsList) {
       $('#fullName').text(doctorsList.data[0].profile.first_name + " " + doctorsList.data[0].profile.last_name);
-      $('#addrress').text(doctorsList.data[0].profile.last_name);
+      $('#street').html(doctorsList.data[0].practices[0].visit_address.street);
+      $('#street2').html(doctorsList.data[0].practices[0].visit_address.street2);
+      $('#zip').html(doctorsList.data[0].practices[0].visit_address.zip);
+      $('#city').html(doctorsList.data[0].practices[0].visit_address.city);
+      $('#state').html(doctorsList.data[0].practices[0].visit_address.state);
       $('#phoneNumber').text(doctorsList.data[0].practices[0].phones[0].number);
       $('#website').text(doctorsList.data[0].profile.last_name);
       $('#newPatients').text(doctorsList.data[0].profile.last_name);
