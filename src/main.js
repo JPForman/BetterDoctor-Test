e9b3cb87434c3c8541b896decacf2775707da321
Joshua Forman <cyborgforman@gmail.com>
2019-11-23 12:32:51 -0800

    refactor logic

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { DoctorService } from './Doctor-Service.js';
-import { OutputElement } from './outputElement.js';
 import { outputElement } from './outputElement.js';
 
 $(document).ready(function(){
@@ -11,35 +10,11 @@ $(document).ready(function(){
     event.preventDefault();
     const issue = $('#issueInput').val();
 
-
     (async () => {
       let doctorService = new DoctorService();
       const doctorsList = await doctorService.getDoctors(issue);
-      // let output = OutputElement.outputElement(doctorsList);
-      let output = outputElement(doctorsList);
+
+      outputElement(doctorsList);
     })();
   });
 });
-
-
-
-// function outputElements(doctorsList) {
-  //   $('#fullName').html(doctorsList.data[0].practices[0].name);
-  //   $('#street').html(doctorsList.data[0].practices[0].visit_address.street);
-  //   $('#street2').html(doctorsList.data[0].practices[0].visit_address.street2);
-  //   $('#zip').html(doctorsList.data[0].practices[0].visit_address.zip);
-  //   $('#city').html(doctorsList.data[0].practices[0].visit_address.city);
-  //   $('#state').html(doctorsList.data[0].practices[0].visit_address.state);
-  //   $('#phoneNumber').text(doctorsList.data[0].practices[0].phones[0].number);
-  //   $('#website').text(doctorsList.data[0].practices[0].website);
-  //   // $('#newPatients').text(doctorsList.data[0].profile.last_name);
-  //   if (doctorsList.data[0].practices[0].accepts_new_patients === true) {
-    //     $('#newPatients').text("This doctor is currently accepting new patients.");
-    //   } else if (doctorsList.data[0].practices[0].accepts_new_patients === false) {
-      //     $('#newPatients').text("Sorry, this doctor is not currently accepting new patients.");
-      //   } else {
-        //     $('#newPatients').text("Sorry, something unexpected happened.");
-        //   }
-        // }
-        //   });
-        // });
--- src/outputElement.js
+++ src/outputElement.js
@@ -1,31 +1,24 @@
 import $ from 'jquery';
-import { DoctorService } from './Doctor-Service.js'
-// export class OutputElement {
-  //
-  //   constructor(doctorList) {
-    //     this.fullName = fullName;
-    //     this.street = street;
-    //     this.street2 = street2;
-    // }
-    export function outputElement(doctorsList) {
-      for (let i=0; i<10; i++) {
-        $('#startPoint').html(`<div id='card${i}' class='jumbotron'>`);
-        $('#fullName').html(doctorsList.data[i].practices[0].name);
-        $('#street').html(doctorsList.data[i].practices[0].visit_address.street);
-        $('#street2').html(doctorsList.data[i].practices[0].visit_address.street2);
-        $('#zip').html(doctorsList.data[i].practices[0].visit_address.zip);
-        $('#city').html(doctorsList.data[i].practices[0].visit_address.city);
-        $('#state').html(doctorsList.data[i].practices[0].visit_address.state);
-        $('#phoneNumber').html(doctorsList.data[i].practices[0].phones[0].number);
-        $('#website').html(doctorsList.data[i].practices[0].website);
-        if (doctorsList.data[i].practices[0].accepts_new_patients === true) {
-          $('#newPatients').html("This doctor is currently accepting new patients.");
-        } else if (doctorsList.data[i].practices[0].accepts_new_patients === false) {
-          $('#newPatients').html("Sorry, this doctor is not currently accepting new patients.");
-        } else {
-          $('#newPatients').html("Sorry, something unexpected happened.");
-        }
-        $('#endPoint').html(`</div>`);
-        return true;
-      }
+
+export function outputElement(doctorsList) {
+  for (let i=0; i<10; i++) {
+    $('#startPoint').html(`<div id='card${i}' class='jumbotron'>`);
+    $('#fullName').html(`<p>${doctorsList.data[i].practices[0].name}</p>`);
+    $('#street').html(doctorsList.data[i].practices[0].visit_address.street);
+    $('#street2').html(doctorsList.data[i].practices[0].visit_address.street2);
+    $('#zip').html(doctorsList.data[i].practices[0].visit_address.zip);
+    $('#city').html(doctorsList.data[i].practices[0].visit_address.city);
+    $('#state').html(doctorsList.data[i].practices[0].visit_address.state);
+    $('#phoneNumber').html(doctorsList.data[i].practices[0].phones[0].number);
+    $('#website').html(doctorsList.data[i].practices[0].website);
+    if (doctorsList.data[i].practices[0].accepts_new_patients === true) {
+      $('#newPatients').html("This doctor is currently accepting new patients.");
+    } else if (doctorsList.data[i].practices[0].accepts_new_patients === false) {
+      $('#newPatients').html("Sorry, this doctor is not currently accepting new patients.");
+    } else {
+      $('#newPatients').html("Sorry, something unexpected happened.");
     }
+    $('#endPoint').html(`</div>`);
+    return true;
+  }
+}
