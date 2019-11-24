35f64186c253d39bc524baf6227ac5b337fcd7ee
Joshua Forman <cyborgforman@gmail.com>
2019-11-23 23:42:57 -0800

    add an ancher tag to websit link in card

--- src/outputElement.js
+++ src/outputElement.js
@@ -3,11 +3,11 @@ import $ from 'jquery';
 export function cardElement(doctorsList) {
   for (let i=0; i<doctorsList.data.length; i++) {
     $('#docOutput').append(`<div class='doctorCard'>
-    <p>${doctorsList.data[i].practices[0].name});</p>
+    <p>${doctorsList.data[i].practices[0].name}</p>
     <p>${doctorsList.data[i].practices[0].visit_address.street}, ${doctorsList.data[i].practices[0].visit_address.street2}</p>
     <p>${doctorsList.data[i].practices[0].visit_address.city}, ${doctorsList.data[i].practices[0].visit_address.state}, ${doctorsList.data[i].practices[0].visit_address.zip}</p>
     <p>${doctorsList.data[i].practices[0].visit_address.number}</p>
-    <p>${doctorsList.data[i].practices[0].website}</p>
+    <p><a href="${doctorsList.data[i].practices[0].website}">${doctorsList.data[i].practices[0].website}</a></p>
 
     </div>`
     );
